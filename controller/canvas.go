cf2d750b71c4f5ab7092891763d0c360a068f7de
YamelYAT <mmoooo@bk.ru>
2020-10-21 01:15:32 +0300

    canvas http handler

--- controller/canvas.go
+++ controller/canvas.go
@@ -1,1 +1,26 @@
 package controller
+
+import (
+	"log"
+	"net/http"
+	"pixelbattle/canvas"
+)
+
+type CanvasController struct {
+	Canvas	*canvas.Canvas
+}
+
+func NewCanvas() *CanvasController{
+	return &CanvasController{Canvas: canvas.NewCanvas()}
+}
+
+func (c *CanvasController)GetCanvas(w http.ResponseWriter, r *http.Request){
+	if r.Method != "GET"{
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+	w.Header().Set("content-type", "text/html; charset=utf-8")
+	w.Header().Set("content-length", "636401")
+	if _, err := w.Write(c.Canvas.Points); err != nil{
+		log.Println(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-package pixelbattle
+package main
+
+import (
+	"log"
+	"net/http"
+	"pixelbattle/controller"
+)
+
+func main(){
+	canvasController := controller.NewCanvas()
+	http.HandleFunc("/", canvasController.GetCanvas)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
