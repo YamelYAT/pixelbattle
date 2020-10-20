5e9d23da55b719c29ba5466f4781aaeff415e650
YamelYAT <mmoooo@bk.ru>
2020-10-21 01:14:22 +0300

    add colors type

--- canvas/colors.go
+++ canvas/colors.go
@@ -1,1 +1,46 @@
 package canvas
+
+import (
+	"math/rand"
+)
+
+type color [25]string
+
+func (c *color)Get(num int) byte{
+	if num >= len(c){
+		return 0
+	}
+	return byte(c[num][0])
+}
+
+func (c *color)GetRandomColor() byte{
+	return byte(c[rand.Intn(25)][0])
+}
+
+var colors = color{
+	"0",
+	"1",
+	"2",
+	"3",
+	"4",
+	"5",
+	"6",
+	"7",
+	"8",
+	"9",
+	"a",
+	"b",
+	"c",
+	"d",
+	"e",
+	"f",
+	"g",
+	"h",
+	"i",
+	"g",
+	"k",
+	"l",
+	"m",
+	"n",
+	"o",
+}
